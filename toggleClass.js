d92ed0b9623d3d68e8da8fbb7a95a7fec7cf1c68
bradkillins <bradkillins@gmail.com>
2022-01-13 17:37:30 -0800

    updated splash image and colours

--- toggleClass.js
+++ toggleClass.js
@@ -1,16 +1,16 @@
 /* classToggle - Toggles a class on/off of a target element when a click 
-                 event occurs on a trigger element
+                 event occurs.
  * @Params - trigger: element selector that will trigger the toggle
  *         - target: element selector of the target
  *         - toggle: the class name to be toggled on/off of target
  */
 
-const classToggle = function(trigger, target, toggles) {
+const classToggle = function (trigger, target, toggles) {
   const triggerClass = document.querySelector(trigger); //get first element with selector equal to trigger
   const targetClass = document.querySelector(target); //get first element with selector equal to target
 
   //detect when click event happens on trigger
-  triggerClass.addEventListener("click", function() {
+  triggerClass.addEventListener("click", function () {
     //add/remove toggle class to target element
     targetClass.classList.toggle(toggles);
   });
